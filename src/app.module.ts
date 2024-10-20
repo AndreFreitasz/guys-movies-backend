8d7144a71ea2a8bd536c57afcd69b6ba6301646e
André Freitas <dre.freitas23@gmail.com>
2024-10-20 15:54:01 -0300

    feat: settings variable environments with dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env.development',
+  })],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
