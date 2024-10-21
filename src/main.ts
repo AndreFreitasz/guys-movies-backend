ef170268be5ad0107362cd0d0b1f6b8b19302dde
André Freitas <dre.freitas23@gmail.com>
2024-10-20 21:24:04 -0300

    fix: remove file `test-db-connection.ts` and cors url

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import helmet from "helmet";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 3000);
-
-  if(process.env.NODE_ENV === "production") {
-    console.log("Production mode enabled");
-    app.use(helmet());
-    app.enableCors({
-      origin: "https://guys-movies-frontend.vercel.app"
-    });
-  }
+  console.log(process.env.PORT);
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Nest server listening");
+  });
 }
 bootstrap();
