5562a47f11e76a2ade7c1234e1ce3f880b152827
AndreFreitaszz <andre.filho@kabum.com.br>
2025-01-16 15:11:23 -0300

    feat: connection with database postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { AppService } from './app.service';
 import { MoviesModule } from 'src/movies/movies.module';
 import * as dotenv from 'dotenv';
 import { SeriesModule } from 'src/series/series.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 dotenv.config();
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
     MoviesModule,
-    SeriesModule
+    SeriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
