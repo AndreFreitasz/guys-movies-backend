4860e7515d915f40baab41c6b5046ef40b132617
André Freitas <dre.freitas23@gmail.com>
2025-01-26 23:33:47 -0300

    refactor: connection database production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ dotenv.config();
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
@@ -25,6 +25,7 @@ dotenv.config();
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         synchronize: true,
+        autoLoadEntities: true
       }),
       inject: [ConfigService],
     }),
